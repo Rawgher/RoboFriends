bb3dd07baed4fb14b6235d18d04dc261e58821f7
Rawgher <rdpender3@gmail.com>
2022-07-30 13:32:41 -0700

    updated react version and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
