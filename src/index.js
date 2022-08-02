03885fa8bb143e6d52d663d79e2fa58f355d7105
Roger Pender <rdpender3@gmail.com>
2022-08-02 10:44:04 -0700

    reverting back from hooks and setting up store/provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider, connect } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import './index.css';
 import App from './containers/App';
 import 'tachyons'; 
+import { searchRobots } from './reducers';
+
+const store = configureStore({reducer: {searchRobots}});
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+);
--- src/reducers.js
+++ src/reducers.js
@@ -7,7 +7,7 @@ const intitialState = {
 export const searchRobots = (state = intitialState, action = {}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField: action.payload});
+            return Object.assign({}, state, { searchField: action.payload });
             // return { ...state, {searchField: action.payload}};
         default:
             return state;
